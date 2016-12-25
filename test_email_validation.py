971ff3d85c5c7907e1a147fede7cfc1b065440f3
Constantine <m04116@hotmail.com>
2016-12-25 13:16:28 +0200

    Fix in travis.yaml and Test_email-validation

--- test_email_validation.py
+++ test_email_validation.py
@@ -5,4 +5,4 @@ import unittest
 class EmailValidationTest(unittest.TestCase):
     """unit test for EmailValidation"""
     def test_validate_email(self):
-        self.assertTrue('123@123.com', email_validation.validate_email())
+        self.assertTrue(email_validation.validate_email('123@123.com'))
