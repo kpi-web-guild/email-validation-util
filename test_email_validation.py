f4cd044b33208eb1b121baead70ffde091e4f3ea
Constantine <m04116@hotmail.com>
2016-12-25 15:42:35 +0200

    .idea, docstrings added

--- test_email_validation.py
+++ test_email_validation.py
@@ -1,8 +1,10 @@
+"""Test module email_validation."""
 from email_validation import validate_email
 import unittest
 
 
 class EmailValidationTest(unittest.TestCase):
-    """Unit test for EmailValidation"""
+    """Unit test for EmailValidation."""
     def test_validate_email(self):
+    	"""Test by assertTrue method."""
         self.assertTrue(validate_email('123@123.com'))
