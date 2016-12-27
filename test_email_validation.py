10287662c183e0505f70673d53b3eac727563394
Constantine <m04116@hotmail.com>
2016-12-27 20:43:55 +0200

    Indentation aligned. All tests are pass.

--- test_email_validation.py
+++ test_email_validation.py
@@ -6,5 +6,5 @@ import unittest
 class EmailValidationTest(unittest.TestCase):
     """Unit test for EmailValidation."""
     def test_validate_email(self):
-    	"""Test by assertTrue method."""
+        """Test by assertTrue method."""
         self.assertTrue(validate_email('123@123.com'))
