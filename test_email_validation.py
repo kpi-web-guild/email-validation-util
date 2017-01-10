afba14db561bb9e70dcb62db3b67031009f06703
Constantine <m04116@hotmail.com>
2017-01-10 20:42:50 +0200

    Correct description of function

--- test_email_validation.py
+++ test_email_validation.py
@@ -4,11 +4,11 @@ import unittest
 
 
 class EmailValidationTest(unittest.TestCase):
-    """Unit tests for email validation module. "When True"."""
+    """Unit tests for email validation module."""
     def test_validate_email(self):
-        """Check whether validate_email function behaves correctly."""
+        """Check whether validate_email function behaves correctly. "When True"."""
         self.assertTrue(validate_email('123@123.com'))
 
     def test_validate_email_2(self):
-        """Unit tests for email validation module. "When False"."""
+        """Check whether validate_email function behaves correctly. "When False"."""
         self.assertFalse(validate_email('АБВ123@123.com'))
