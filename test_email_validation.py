d0322582c56b2e21daefec2339f6b82672013309
Constantine <m04116@hotmail.com>
2017-01-10 20:30:06 +0200

    Add positive (valid emails passed) and negative (invalid emails passed) tests.

--- test_email_validation.py
+++ test_email_validation.py
@@ -4,7 +4,11 @@ import unittest
 
 
 class EmailValidationTest(unittest.TestCase):
-    """Unit tests for email validation module."""
+    """Unit tests for email validation module. "When True"."""
     def test_validate_email(self):
         """Check whether validate_email function behaves correctly."""
         self.assertTrue(validate_email('123@123.com'))
+
+    def test_validate_email_2(self):
+        """Unit tests for email validation module. "When False"."""
+        self.assertFalse(validate_email('АБВ123@123.com'))
