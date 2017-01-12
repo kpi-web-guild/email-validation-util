2d475de87e14c6655b1032a196fa8e394bc65f43
Constantine <m04116@hotmail.com>
2017-01-12 22:13:46 +0200

    Rename docstrings.

--- test_email_validation.py
+++ test_email_validation.py
@@ -5,10 +5,10 @@ import unittest
 
 class EmailValidationTest(unittest.TestCase):
     """Unit tests for email validation module."""
-    def test_validate_email(self):
-        """Check whether validate_email function behaves correctly. "When True"."""
+    def test_validate_email_pass(self):
+        """Check whether validate_email function pass."""
         self.assertTrue(validate_email('123@123.com'))
 
-    def test_validate_email_2(self):
-        """Check whether validate_email function behaves correctly. "When False"."""
+    def test_validate_email_false(self):
+        """Check whether validate_email function false."""
         self.assertFalse(validate_email('АБВ123@123.com'))
