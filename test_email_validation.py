81776f4daa5c794d9d3ecaaf2d87e0c120f702c4
Constantine <m04116@hotmail.com>
2017-01-16 13:38:33 +0000

    Add drafts to test method

--- test_email_validation.py
+++ test_email_validation.py
@@ -8,7 +8,21 @@ class EmailValidationTest(unittest.TestCase):
     def test_validate_email_pass(self):
         """Check whether validate_email function pass."""
         self.assertTrue(validate_email('123@123.com'))
+        self.assertTrue(validate_email('abc@abc.com'))
+        self.assertTrue(validate_email('abc123@abc123.com'))
+        self.assertTrue(validate_email('a.bc_123@abc123.com'))
+
 
     def test_validate_email_false(self):
         """Check whether validate_email function false."""
         self.assertFalse(validate_email('АБВ123@123.com'))
+        self.assertTrue(validate_email('123@абв.com'))
+        self.assertTrue(validate_email('1..23@abc.com'))
+        self.assertTrue(validate_email('1-23@abc.com'))
+        self.assertTrue(validate_email('1/23abc@abc.com'))
+        self.assertTrue(validate_email('1+23@abc.com'))
+        self.assertTrue(validate_email('1\23@abc.com'))
+        self.assertTrue(validate_email('1!23abc@abc.com'))
+        self.assertTrue(validate_email('1*23abc@abc.com'))
+        self.assertTrue(validate_email('1?23abc@abc.com'))
+
